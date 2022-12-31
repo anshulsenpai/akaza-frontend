03ddef891636205f9afa213fef3dfd877b9efb88
Anshul Kulkarni <anshulkulkarni00@gmail.com>
2022-12-31 19:57:05 +0530

    changed base URL

--- src/ReqMethods.jsx
+++ src/ReqMethods.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const BASE_URL = "http://localhost:8000"
+export const BASE_URL = "https://akaza-games-api.onrender.com"
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMDFiY2U2ZGYxY2Q3YjM3NzM0ZDg2NiIsImlhdCI6MTY2MTA2NTkxMiwiZXhwIjoxNjYyMzYxOTEyfQ.mKIFr2K3YJ5F4bkqhg4F2lYrNpiAa2NJYeBbeN8d_Ow"
 
 export const publicRequest = axios.create({
